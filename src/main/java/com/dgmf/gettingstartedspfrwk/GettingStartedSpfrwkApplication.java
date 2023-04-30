19e7a65cf355a388380fa763f533f5c273709567
FranckDodo91 <devlogappdgmf@gmail.com>
2023-04-30 08:05:49 +0200

    Project initialization

---
+++ src/main/java/com/dgmf/gettingstartedspfrwk/GettingStartedSpfrwkApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.gettingstartedspfrwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedSpfrwkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedSpfrwkApplication.class, args);
+	}
+
+}
