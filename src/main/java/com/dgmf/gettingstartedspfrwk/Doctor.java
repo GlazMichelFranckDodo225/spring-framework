2d5609f1439268410afd1d7a83121aa994d44aa7
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-03 04:25:47 +0200

    Creation of the Doctor class and call of the assist() method of its instance in the Main driver class

---
+++ src/main/java/com/dgmf/gettingstartedspfrwk/Doctor.java
@@ -0,0 +1,7 @@
+package com.dgmf.gettingstartedspfrwk;
+
+public class Doctor {
+    public void assist() {
+        System.out.println("Doctor is assisting ...");
+    }
+}
--- src/main/java/com/dgmf/gettingstartedspfrwk/Main.java
+++ src/main/java/com/dgmf/gettingstartedspfrwk/Main.java
@@ -3,11 +3,12 @@ package com.dgmf.gettingstartedspfrwk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GettingStartedSpfrwkApplication {
+
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GettingStartedSpfrwkApplication.class, args);
+		Doctor doctor = new Doctor();
+		doctor.assist();
 	}
 
 }
