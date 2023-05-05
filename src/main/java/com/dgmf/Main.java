3d2f6386106b5700ed0ae6b15aaf48029ce9194d
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-05 06:26:55 +0200

    Spring IoC instantiates only one Bean for each class

--- src/main/java/com/dgmf/Main.java
+++ src/main/java/com/dgmf/Main.java
@@ -12,9 +12,21 @@ public class Main {
 		ApplicationContext context =
 				new ClassPathXmlApplicationContext("spring.xml");
 
-		// Nurse nurse = context.getBean(Nurse.class);
-		Nurse nurse = (Nurse) context.getBean("nurse");
+		Staff doctor = context.getBean(Doctor.class);
+		Staff staff1 = context.getBean(Doctor.class);
+		doctor.assist();
+		staff1.assist();
+
+		Staff nurse = context.getBean(Nurse.class);
+		Staff staff2 = context.getBean(Nurse.class);
 		nurse.assist();
+		staff2.assist();
+
+
+		System.out.println(doctor); // com.dgmf.Doctor@5fdcaa40
+		System.out.println(staff1); // com.dgmf.Doctor@5fdcaa40
+		System.out.println(nurse); // com.dgmf.Nurse@6dc17b83
+		System.out.println(staff2); // com.dgmf.Nurse@6dc17b83
 	}
 
 }
