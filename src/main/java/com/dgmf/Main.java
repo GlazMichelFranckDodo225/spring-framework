0e5cd1af6c9d06b0cd6296e79df33287305c1f9f
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-22 13:24:54 +0200

    Refactoring to implement configuration by annotation

--- src/main/java/com/dgmf/Main.java
+++ src/main/java/com/dgmf/Main.java
@@ -7,14 +7,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
 	public static void main(String[] args) {
-		// Instanciation du conteneur IoC (basée sur une
-		// configuration XML)
 		ApplicationContext context =
 				new ClassPathXmlApplicationContext("spring.xml");
 
-		Doctor doctor = context.getBean(Doctor.class);
-		System.out.println(doctor.getQualification()); // MBBS
-		// System.out.println(doctor.getNurse()); // com.dgmf.Nurse@71d44a3
+		Doctor staff = context.getBean(Doctor.class);
+		staff.assist();
 	}
 
 }
